56504815f8cf6889b9b964da93bd9802594add7a
Evils <evils.devils@protonmail.com>
2019-01-18 00:14:58 +0100

    s/packet/frame/g

--- main.c
+++ main.c
@@ -4,32 +4,29 @@
 #include <pcap.h>
 
 
-// get MAC and IP addresses out of a packet (first 66 bytes should have this)
-struct addrss get_addrss(packet)
+// get MAC and IP addresses out of a frame (first 66 bytes should have this)
+struct addrss get_addrss(frame)
 {
 	struct addrss addrss;
 	int offset = 0;
 
 	// network to host byte order
-	packet = ntohs(packet);
+	frame = ntohs(frame);
 
-	// check packet validity
-	// getting just 66 bytes may be more efficient but prohibits this
-	if (invalid(packet))
-	{
-		warn ("found invalid packet");
-		return -1;
-	}
+	// TODO
+	// just get the head of the frame, assume it's right,
+	// MAC will be unreachable and get purged
+	// check if the IP is in the right range later?
 
 	// doesn't match offsets in dsniff/pcaputil.c
-	switch (pcap_datalink(packet))
+	switch (pcap_datalink(frame))
 	{
 		case DLT_EN10MB:
 
 			offset += 8;
-			addrss.mac.source = get_mac(packet, offset);
+			addrss.mac.source = get_mac(frame, offset);
 			offset += 6;
-			addrss.mac.dest = get_mac(packet, offset);
+			addrss.mac.dest = get_mac(frame, offset);
 
 			// skip to payload
 			offset += 8;
@@ -37,7 +34,7 @@ struct addrss get_addrss(packet)
 			// set offset based on payload type
 			// get_eth_type() should take 802.1Q in mind
 			// source IP address should be sufficient
-			switch (get_eth_type(packet))
+			switch (get_eth_type(frame))
 			{
 				case IPv4:
 					addrss.ipv = 4;
@@ -64,22 +61,22 @@ struct addrss get_addrss(packet)
 					warn ("unknown ethernet type");
 					return -1;
 
-					get_ip(packet, &addrss);
+					get_ip(frame, &addrss);
 					return 0;
 			}
 
 		case DLT_IEEE802_11:
 
-			switch (Get_frame_type(packet))
+			switch (get_frame_type(frame))
 			{
 				case data_frame:
-					addrss.mac.source = get_mac(packet,
+					addrss.mac.source = get_mac(frame,
 					offset += 4);
-					//addrss.mac.dest = get_mac(packet,
+					//addrss.mac.dest = get_mac(frame,
 					//offset += 6);
-					addrss.mac.tx_sta= get_mac(packet,
+					addrss.mac.tx_sta= get_mac(frame,
 					offset += 6);
-					//addrss.mac.rx_sta= get_mac(packet,
+					//addrss.mac.rx_sta= get_mac(frame,
 					//offset += 8);
 
 				//TODO, other cases?
