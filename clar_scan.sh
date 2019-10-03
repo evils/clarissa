5579fb48f5157e604d3438b7ae311e8be0d98702
Evils <evils.devils@protonmail.com>
2019-10-03 15:36:57 +0200

    clar scan 2 line formatting
    also fall back to /tmp/clar_*

--- clar_scan.sh
+++ clar_scan.sh
@@ -1,17 +1,22 @@
-#/usr/bin/env sh
+#!/usr/bin/env sh
 
 echo
 
-TALLY=0
+if [ -z "$1" ]; then
+	echo "No input file specified."
+	set $1 "$(echo /tmp/clar_*)"
+	echo "Falling back on $1"
+	echo
+fi
 
 while read -r; do
-	ip="$(echo $REPLY | awk '{print $2}')"
-	mac="$(echo $REPLY | awk '{print $1}')"
-	vend_mac="$(echo $REPLY | tr -d ":-" | tr [a-f] [A-F] | awk '{print substr($1,1,6)}')"
-	vendor="$(grep $vend_mac oui.txt | awk '{ s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }')"
+	ip="$(echo "$REPLY" | awk '{print $2}')"
+	mac="$(echo "$REPLY" | awk '{print $1}')"
+	vend_mac="$(echo "$REPLY" | tr -d ":-" | tr "a-f" "A-F" | awk '{print substr($1,1,6)}')"
+	vendor="$(grep "$vend_mac" oui.txt | cut -f 3- )"
 	if [ -z "$vendor" ]; then vendor="(unknown)"; fi
-	printf " $ip\t\t\t\t$mac\t$vendor\n"
-	(( TALLY++ ))
+	printf " %s\t%s\n" "$mac" "$vendor"
+	if [ "$ip" ]; then printf " %s\n\n" "$ip"; fi
 done < "$1"
 
-printf "\n found $TALLY devices\n\n"
+echo
