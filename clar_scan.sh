c899307eae6c8ff5b81a679f2636c631c68e328a
Evils <evils.devils@protonmail.com>
2019-10-04 19:53:28 +0200

    clar scan: condense output
    no blank lines in main block
    	indent IP with 1 space after MAC/vendor line
    
    clar: use exec

--- clar_scan.sh
+++ clar_scan.sh
@@ -3,8 +3,8 @@
 echo
 
 if [ -z "$1" ]; then
-	echo "No input file specified."
 	set $1 "$(echo /tmp/clar_*)"
+	echo "No input file specified."
 	echo "Falling back on $1"
 	echo
 fi
@@ -15,8 +15,8 @@ while read -r; do
 	vend_mac="$(echo "$REPLY" | tr -d ":-" | tr "a-f" "A-F" | awk '{print substr($1,1,6)}')"
 	vendor="$(grep "$vend_mac" oui.txt | cut -f 3- )"
 	if [ -z "$vendor" ]; then vendor="(unknown)"; fi
-	printf " %s\t%s\n" "$mac" "$vendor"
-	if [ "$ip" ]; then printf " %s\n\n" "$ip"; fi
+	printf "%s\t%s\n" "$mac" "$vendor"
+	if [ "$ip" ]; then echo " $ip"; fi
 done < "$1"
 
 echo
