de400e6224e090138b5f582f7de16332e203911a
Evils <evils.devils@protonmail.com>
2019-10-24 22:30:15 +0200

    retry in OUI_assemble.sh
    shellcheck clar_show.sh
    	no more bash array
    adjust clar_show.sh to match clar_OUI.csv format

--- clar_show.sh
+++ clar_show.sh
@@ -15,7 +15,7 @@ while read -r; do
 	ip="$(echo "$REPLY" | awk '{print $2}')"
 	mac="$(echo "$REPLY" | awk '{print $1}')"
 	vend_mac="$(echo "$REPLY" | tr -d ":-" | tr "a-f" "A-F" | awk '{print substr($1,1,6)}')"
-	vendor="$(grep "$vend_mac" $oui | awk -F ',' '{print $3}' | sed 's/"//g' )"
+	vendor="$(grep "$vend_mac" $oui | awk -F ',' '{print $2}' | sed 's/"//g' )"
 	if [ -z "$vendor" ]; then vendor="(unknown)"; fi
 	printf "%s\t%s\n" "$mac" "$vendor"
 	if [ "$ip" ]; then echo " $ip"; fi
