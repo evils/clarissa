6163ea477a2d6a2e5b71a77403808b9ccdd5d528
Evils <evils.devils@protonmail.com>
2019-12-25 18:11:04 +0000

    shellcheck

--- clar_show.sh
+++ clar_show.sh
@@ -5,6 +5,7 @@ oui=clar_OUI.csv
 echo
 
 if [ -z "$1" ]; then
+	# shellcheck disable=SC2086
 	set $1 "$(echo /tmp/clar_*)"
 	echo "No input file specified."
 	echo "Falling back on $1"
@@ -12,16 +13,16 @@ if [ -z "$1" ]; then
 fi
 
 echo "format is:"
-printf "MAC\tvendor\n  IPv4\t  domain\t  IPv6\n"
+printf "MAC\\tvendor\\n  IPv4\\t  domain\\t  IPv6\\n"
 echo
 
-printf "Interface: %s\n" "$(echo "$1" | awk -F '_' '{print $2}')"
+printf "Interface: %s\\n" "$(echo "$1" | awk -F '_' '{print $2}')"
 count=$(wc -l "$1" | awk '{print $1}')
 printf "Clarissa found %s device" "$count"
-if [ $count -gt 1 ]; then printf "s"; fi
-printf "\n\n"
+if [ "$count" -gt 1 ]; then printf "s"; fi
+printf "\\n\\n"
 
-while read -r; do
+while read -r "REPLY"; do
 	mac="$(echo "$REPLY" | awk '{print $1}')"
 	vend_mac="$(echo "$REPLY" | tr -d ":-" | tr "a-f" "A-F" | awk '{print substr($1,1,6)}')"
 	vendor="$(grep "$vend_mac" $oui | awk -F ',' '{print $2}' | sed 's/"//g' )"
@@ -31,16 +32,17 @@ while read -r; do
 	domain="$(dig -x "$ipv4" | grep "ANSWER SECTION" -A 1 | awk '{print substr($5, 1, length($5)-1)}' | sed '/^\s*$/d')"
 
 	if [ -z "$vendor" ]; then vendor="(unknown)"; fi
-	printf "%s\t%s\n" "$mac" "$vendor"
-	printf "  %s\t\t" "$ipv4"
+	printf "%s\\t%s\\n" "$mac" "$vendor"
+	printf "  %s\\t\\t" "$ipv4"
 	if [ "$domain" ]; then
 		printf "  %s" "$domain"
-		if [ $(echo "$domain" | wc -c) -le 12 ]; then
-			printf "\t"
+		# shellcheck disable=SC2000
+		if [ "$(echo "$domain" | wc -c)" -le 12 ]; then
+			printf "\\t"
 		fi
-	else printf "\t\t"
+	else printf "\\t\\t"
 	fi
-	printf "\t  %s\n" "$ipv6"
+	printf "\\t  %s\\n" "$ipv6"
 done < "$1"
 
 echo
