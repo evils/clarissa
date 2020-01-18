07afcc6607594f709ab9cbda19ba8c7cf21f97b9
Evils <evils.devils@protonmail.com>
2020-01-18 01:10:43 +0000

    show: sort output
    emmit more specific notices
      for MAC address, on LAA and unknown UAA
      for domain if not found
    fall back on IPv6 for reverse DNS lookup
    specify output format in comments

--- clar_show.sh
+++ clar_show.sh
@@ -13,6 +13,10 @@ if [ -z "$1" ]; then
 fi
 
 echo "format is:"
+# notices in parenthesis
+# no spaces in individual strings
+#   vendor string can't be helped
+#   so put that on the end of a line!
 printf "MAC\\tvendor\\n  IPv4\\t  domain\\t  IPv6\\n"
 echo
 
@@ -22,16 +26,34 @@ printf "Clarissa found %s device" "$count"
 if [ "$count" -gt 1 ]; then printf "s"; fi
 printf "\\n\\n"
 
-while read -r "REPLY"; do
+sort "$1" \
+| while read -r "REPLY"; do
 	mac="$(echo "$REPLY" | awk '{print $1}')"
-	vend_mac="$(echo "$REPLY" | tr -d ":-" | tr "a-f" "A-F" | awk '{print substr($1,1,6)}')"
-	vendor="$(grep "$vend_mac" $oui | awk -F ',' '{print $2}' | sed 's/"//g' )"
-
+	vend_mac="$(echo "$REPLY" | tr -d ":-" \
+		| tr "a-f" "A-F" \
+		| awk '{print substr($1,1,6)}')"
+	vendor="$(grep "$vend_mac" $oui \
+		| awk -F ',' '{print $2}' | sed 's/"//g' )"
 	ipv4="$(echo "$REPLY" | awk '{print $2}')"
 	ipv6="$(echo "$REPLY" | awk '{print $3}')"
-	domain="$(dig -x "$ipv4" | grep "ANSWER SECTION" -A 1 | awk '{print substr($5, 1, length($5)-1)}' | sed '/^\s*$/d')"
-
-	if [ -z "$vendor" ]; then vendor="(unknown)"; fi
+	domain="$(dig -x "$ipv4" | grep "ANSWER SECTION" -A 1 \
+		| awk '{print substr($5, 1, length($5)-1)}' \
+		| sed '/^\s*$/d')"
+	if [ -z "$domain" ]; then
+		domain="$(dig -x "$ipv6" | grep "ANSWER SECTION" -A 1 \
+			| awk '{print substr($5, 1, length($5)-1)}' \
+			| sed '/^\s*$/d')"
+	fi
+	if [ -z "$domain" ]; then domain="(no_domain_found)"; fi
+        if [ -z "$vendor" ]; then
+                byte2="$(echo "$vend_mac" | cut -b 2)"
+                result="$(( ( "$byte2" / 2 ) % 2 ))"
+                if [ "$result" -eq 1 ]; then
+                        vendor="$(printf "(Locally_Administered_Address)")"
+		else
+                        vendor="$(printf "(vendor_not_listed_(UAA))")"
+                fi
+        fi
 	printf "%s\\t%s\\n" "$mac" "$vendor"
 	printf "  %s\\t\\t" "$ipv4"
 	if [ "$domain" ]; then
@@ -43,6 +65,6 @@ while read -r "REPLY"; do
 	else printf "\\t\\t"
 	fi
 	printf "\\t  %s\\n" "$ipv6"
-done < "$1"
+done
 
 echo
